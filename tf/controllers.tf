f8f678a706b16ccebfa9b86ea0e4d8e5d8ba0add
Michael Asnes <mike.asnes@gmail.com>
2020-10-27 21:54:54 -0600

    pre terraform apply

--- tf/controllers.tf
+++ tf/controllers.tf
@@ -1,22 +1,22 @@
 resource "aws_instance" "controller" {
   count = 3
 
-  ami = "ami-032ed93ef3b2a867c"  # Ubuntu 20.04 amd64
+  ami = "ami-032ed93ef3b2a867c" # Ubuntu 20.04 amd64
 
-  instance_type = "t2.nano"  # TODO: this will need to be beefed up but keep it cheap for now
+  instance_type = "t2.nano" # TODO: this will need to be beefed up but keep it cheap for now
 
   subnet_id = aws_subnet.k8s.id
 
-  source_dest_check = false  # Equivalent to '--can-ip-forward' in gcloud
-  private_ip = "10.240.0.1${count.index}"
+  source_dest_check = false # Equivalent to '--can-ip-forward' in gcloud
+  private_ip        = "10.240.0.1${count.index}"
 
   root_block_device {
     volume_size = 200
   }
 
   tags = {
-    controller = "true"
+    controller              = "true"
     kubernetes-the-hard-way = "true"
-    type = "controller"
+    type                    = "controller"
   }
 }
